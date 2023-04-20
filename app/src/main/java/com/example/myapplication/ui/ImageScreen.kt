44aef1d8993799f605dce1f66d0b7bd645cc1bc2
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-04-20 21:49:13 +0700

    Complete App screen

--- app/src/main/java/com/example/myapplication/ui/ImageScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/ImageScreen.kt
@@ -6,45 +6,68 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.vector.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.*
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.example.myapplication.ImageTypeBird
-import com.example.myapplication.ImageTypeCat
-import com.example.myapplication.ImageTypeDog
-import com.example.myapplication.imageTypes
-import com.example.myapplication.ui.ImageViewModel
+import com.example.myapplication.data.ImgTypes
 
 sealed class ImageType(val label: String)
 
-val imageTypes = listOf(ImageType("Car"), ImageType("Country"), ImageTypeBird)
 @Preview(showBackground = true)
 @Composable
 fun ImageMainScreen(modifier: Modifier = Modifier,
                     imgViewModel: ImageViewModel = viewModel()
 ) {
+    val imageUiState by imgViewModel.uiState.collectAsState()
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(8.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        val imageUiState by imgViewModel.uiState.collectAsState()
-        ImageInputLayout(isValid = false)
+        Text(text = "Search random images",
+        fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+            )
+        Text(text = "Image must be larger than 200x200 pixel and not larger than 1000x1000 pixels",
+        fontSize = 16.sp,
+            color = Color.Gray
+            )
+        ImageInputLayout(
+            imageUiState.isWidthValid,
+            imageUiState.isHeightValid,
+            imgViewModel,
+            imgViewModel.width,
+            imgViewModel.height,
+            imgViewModel.type,
+            { imgViewModel.updateWidth(it)},
+            { imgViewModel.updateHeight(it)},
+            { imgViewModel.updateImageType(it)}
+            )
     }
 }
 
 @Composable
 fun ImageInputLayout(
-    isValid: Boolean
+    isWidthValid: Boolean,
+    isHeightValid: Boolean,
+    imgViewModel: ImageViewModel,
+    width: String,
+    height: String,
+    imgType: String,
+    onWidthChange: (String) -> Unit,
+    onHeightChange: (String) -> Unit,
+    onImageTypeChange: (String) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -53,66 +76,64 @@ fun ImageInputLayout(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row() {
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Image size (1-999)") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = "",
-                onValueChange = {
-
-                },
-                label = { Text("Image size (1-999)") },
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        DropdownMenu(
-            expanded = false,
-            onDismissRequest = {},
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            imageTypes.forEach { type ->
-                DropdownMenuItem(onClick = { imageType = type }) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            imageVector = type.image,
-                            contentDescription = type.label,
-                            tint = Color.Unspecified,
-                            modifier = Modifier.size(24.dp)
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(type.label)
+        OutlinedTextField(
+            value = width,
+            onValueChange = onWidthChange,
+            isError =  !isWidthValid,
+            label = {
+                    if(isWidthValid) {
+                        Text("Width (pixels)")
                     }
-                }
+                else {
+                        Text("Width out of scope!")
+                    }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = height,
+            onValueChange = onHeightChange,
+            isError = !isHeightValid,
+            label = {
+                if(isHeightValid) {
+                Text("Height (pixels)")
             }
-        }
-        RandomImageButton(imgX = 60, imgY = 60, imageType = "Your mom")
+            else {
+                Text("Height out of scope!")
+            } },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = imgType,
+            onValueChange = onImageTypeChange,
+            label = { Text("Image Type (Can be empty)") },
+            keyboardOptions = KeyboardOptions.Default,
+            modifier = Modifier.fillMaxWidth()
+        )
+        RandomImageButton({imgViewModel.imgValidation(width.toInt(), height.toInt())})
+        DisplayImage(url = imgViewModel.url)
     }
 }
 @Composable
 fun RandomImageButton(
-    imgX: Int = 0,
-    imgY: Int = 0,
-    imageType: String,
-    view: ImageViewModel,
+    validation: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Button(
-        onClick = {
-            view.imgValidation(imgX, imgY)
-        },
-        modifier = Modifier.fillMaxWidth()
+        onClick = validation,
+        modifier = Modifier.fillMaxWidth().padding(8.dp)
     ) {
-        Text("Show Image")
+        Text("Show Image", fontSize = 20.sp)
     }
 }
 
 @Composable
-fun DisplayImage(imgX: Int, imgY: Int, imageType: String) {
+fun DisplayImage(url: String) {
     AsyncImage(model = ImageRequest.Builder(LocalContext.current)
-        .data("https://loremflickr.com/$imgX/$imgY/$imageType"),
-        contentDescription = "An image of $imageType with $imgX width and $imgY height")
+        .data(url)
+        .crossfade(true)
+        .build(),
+        contentDescription = "")
 }
