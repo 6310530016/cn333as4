e0559d9be9b889ca478ad0c530a47dba11ec8fa9
Supachai Matsuwannakit <6310530016@cn330>
2023-04-20 15:19:17 +0700

    Edited TypeData.kt

--- app/src/main/java/com/example/myapplication/data/TypeData.kt
+++ app/src/main/java/com/example/myapplication/data/TypeData.kt
@@ -3,4 +3,12 @@ val allType: Set<ImageType> =
     setOf(
         ImageType("Man"),
         ImageType("Woman"),
+        ImageType("girl"),
+        ImageType("boy"),
+        ImageType("eagle"),
+        ImageType("Devil"),
+        ImageType("Angle"),
+        ImageType("Shark"),
+        ImageType("Owส"),
+        ImageType("Snake"),
     )
