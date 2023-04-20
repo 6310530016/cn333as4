eef88070d14e6d4181a5af03bbdb804712071a71
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-04-20 21:47:46 +0700

    Using UI State

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,20 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.vector.*
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.*
-import com.example.myapplication.data.ImageType
 import com.example.myapplication.ui.ImageMainScreen
 
 class MainActivity : ComponentActivity() {
@@ -39,13 +27,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-sealed class ImageType(val label: String, val image: ImageVector)
-object ImageTypeCat : ImageType("Cat", ImageVector.vectorResource(R.drawable.ic_cat))
-object ImageTypeDog : ImageType("Dog", ImageVector.vectorResource(R.drawable.ic_dog))
-object ImageTypeBird : ImageType("Bird", ImageVector.vectorResource(R.drawable.ic_bird))
-
-val imageTypes = listOf(ImageTypeCat, ImageTypeDog, ImageTypeBird)
-
+/*
 @Composable
 fun RandomImageScreen() {
     var imageSize by remember { mutableStateOf("") }
@@ -78,3 +60,4 @@ fun RandomImageScreen() {
         }
     }
 }
+ */
